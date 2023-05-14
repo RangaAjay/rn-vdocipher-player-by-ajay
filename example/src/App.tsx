5b7c5af3533ef25f31b88bef367cf1c6d00e18f2
Ajay <ajay.ranga@hcode.tech>
2023-05-14 21:16:48 +0530

    minor changes in tsx

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,34 +1,23 @@
 import * as React from 'react';
 
-import { StyleSheet, View } from 'react-native';
+import { SafeAreaView, StyleSheet } from 'react-native';
 import { RnVdocipherPlayerByAjayView } from 'rn-vdocipher-player-by-ajay';
 
 export default function App() {
-  const [counter, setCounter] = React.useState<number>(0);
   return (
-    <View style={styles.container}>
-      <RnVdocipherPlayerByAjayView
-        color="black"
-        style={styles.box}
-        value={counter}
-        leftButtonText={'Minus'}
-        rightButtonText={'Plus'}
-        onPressLeftButton={() => setCounter(counter - 1)}
-        onPressRightButton={() => setCounter(counter + 1)}
-      />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <RnVdocipherPlayerByAjayView style={styles.box} />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
   box: {
     width: '100%',
-    height: 60,
-    marginVertical: 20,
+    height: 600,
+    backgroundColor: 'skyblue',
   },
 });
