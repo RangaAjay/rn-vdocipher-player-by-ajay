962b0e2a05f4a20d2db48307ee1345eb67917b4a
Ajay <ajay.ranga@hcode.tech>
2023-05-14 19:55:46 +0530

    story board added

--- ios/RnVdocipherPlayerByAjayViewManager.m
+++ ios/RnVdocipherPlayerByAjayViewManager.m
@@ -3,11 +3,5 @@
  
 @interface RCT_EXTERN_MODULE(VideoPlayerViewManager, RCTViewManager)
 
-RCT_EXPORT_VIEW_PROPERTY(value, NSNumber)
-RCT_EXPORT_VIEW_PROPERTY(setValue, NSNumber)
-RCT_EXPORT_VIEW_PROPERTY(leftButtonText, NSString)
-RCT_EXPORT_VIEW_PROPERTY(rightButtonText, NSString)
-RCT_EXPORT_VIEW_PROPERTY(onPressLeftButton, RCTDirectEventBlock)
-RCT_EXPORT_VIEW_PROPERTY(onPressRightButton, RCTDirectEventBlock)
 
 @end
--- ios/RnVdocipherPlayerByAjayViewManager.swift
+++ ios/RnVdocipherPlayerByAjayViewManager.swift
@@ -1,4 +1,5 @@
 import Foundation
+import SwiftUI
 
 @objc (VideoPlayerViewManager)
 class VideoPlayerViewManager: RCTViewManager {
@@ -25,106 +26,6 @@ class VideoPlayerView : UIView {
     }
     
     private func setupView() {
-        self.addSubview(valueLabel)
-        self.addSubview(buttonsView)
-        
-        buttonsView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 8)
-            .isActive = true
-        buttonsView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -8)
-            .isActive = true
-        buttonsView.heightAnchor.constraint(equalToConstant: 48)
-            .isActive = true
-        buttonsView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -8)
-            .isActive = true
-        
-        buttonsView.addArrangedSubview(leftButton)
-        buttonsView.addArrangedSubview(rightButton)
-        
-        setupEvents()
-    }
-    
-    private func setupEvents() {
-        let leftButtonTap = UITapGestureRecognizer(
-            target: self,
-            action: #selector(pressLeftButton)
-        )
-        leftButton.addGestureRecognizer(leftButtonTap)
-        
-        let rightButtonTap = UITapGestureRecognizer(
-            target: self,
-            action: #selector(pressRightButton)
-        )
-        rightButton.addGestureRecognizer(rightButtonTap)
-    }
-    
-    
-    
-    let valueLabel: UILabel = {
-        let label = UILabel()
-        label.text = "0"
-        label.textAlignment = .center
-        label.font = label.font.withSize(36)
-        label.textColor=UIColor.red
-        label.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        return label
-    }()
-    
-    let buttonsView: UIStackView = {
-        let view = UIStackView()
-        view.axis = .horizontal
-        view.distribution = .fillEqually
-        view.spacing = 16
-        view.alignment = .fill
-        view.translatesAutoresizingMaskIntoConstraints = false
-        return view
-    }()
-    
-    let leftButton: UIButton = {
-        let button = UIButton()
-        button.setTitleColor(.black, for: .normal)
-        button.setTitleColor(.gray, for: .highlighted)
-        button.setTitle("Button", for: .normal)
-//        button.backgroundColor=UIColor.red
-        return button
-    }()
-    
-    let rightButton: UIButton = {
-        let button = UIButton()
-        button.setTitleColor(.black, for: .normal)
-        button.setTitleColor(.gray, for: .highlighted)
-        button.setTitle("Button", for: .normal)
-//        button.backgroundColor=UIColor.green
-        return button
-    }()
-    
-    @objc func setValue(_ val: NSNumber) {
-        valueLabel.text = val.stringValue
-    }
-    
-    @objc func setLeftButtonText(_ val: NSString) {
-        leftButton.setTitle(val as String, for: .normal)
-    }
-    
-    @objc func setRightButtonText(_ val: NSString) {
-        rightButton.setTitle(val as String, for: .normal)
-    }
-    
-    @objc var onPressLeftButton: RCTDirectEventBlock?
-    @objc var onPressRightButton: RCTDirectEventBlock?
-    
-    @objc func pressLeftButton(sender: UIButton) {
-        if onPressLeftButton == nil {
-            return
-        }
-        let event = [AnyHashable: Any]()
-        onPressLeftButton!(event)
-    }
-    
-    @objc func pressRightButton(sender: UIButton) {
-        if onPressRightButton == nil {
-            return
-        }
-        let event = ["message": "hello world"]
-        onPressRightButton!(event)
+        self.addSubview(ViewController().view)
     }
 }
